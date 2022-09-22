c27abe47526a685cff9fb1d9b3312bfb16cfd261
bernard chisumo <bchisumo74@gmail.com>
2022-09-22 15:25:27 +0100

    Configured the router/index.js file + web

--- pages.md
+++ pages.md
@@ -1,4 +1,8 @@
     ### SETUP PAGES IN COMPONENTS FOLDER
         create a pages folder inside components
-            pages->home->index.vue
-            pages->admin->index.vue
+            pages->home->index.vue file 
+            pages->admin->index.vue file 
+            components->noFound.vue file 
+
+    ### CREATE A ROUTER FOLDER IN JS FOLDEER
+            
---
+++ resources/js/router/index.js
@@ -0,0 +1,36 @@
+import { createRouter,createWebHistory} from "vue-router";
+
+/** Admin Dashboard*/
+import homeAdminIndex  from  '../components/admin/home/index.vue';
+
+/** Import Pages*/
+import homePageIndex  from  '../components/pages/home/index.vue';
+
+/** Import Not Found*/
+import notFound from "../components/notFound.vue";
+
+const  routes = [
+    /** admin*/
+    {
+        path: '/admin/home',
+        component: homeAdminIndex
+    },
+    /** pages*/
+    {
+        path: '/',
+        component: homePageIndex
+    },
+    /** notFound */
+    {
+        path: '/:pathMatch(.*)*',
+        component: notFound
+    }
+
+]
+
+const  router = createRouter({
+    history : createWebHistory(process.env.BASE_URL),
+    routes,
+})
+
+export  default router;
